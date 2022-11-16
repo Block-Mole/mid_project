757274edeab1f47156267e03ee238df88cc2bbc2
Jaime <jaimevs1993@gmail.com>
2022-11-16 12:02:02 -0600

    api_inicio

--- api/main.py
+++ api/main.py
@@ -6,3 +6,13 @@ def inicio():
     return{
         "message": "prueba 1"
     }
+@app.get("/query")
+def query(name):
+    return {
+        "name":name
+    }
+@app.get("saludar/{name}")
+def saludar(name):
+    return {
+        "saludo": name
+    }
