ec33626f2e9628e23773385426b0757ca3aaebb0
Jaime <jaimevs1993@gmail.com>
2022-11-17 05:11:38 -0600

    api_update

--- api/Routers/acceso.py
+++ api/Routers/acceso.py
@@ -1,28 +1,33 @@
 from fastapi import APIRouter
-from Database.mongodb import db
-from bson import json_util #transformar json porque al hacer la llamada nos da un bson mo un json
+from Database.mongodb import db #estamos entrando en la carpeta Database, el file mongodb, y de ahi obteniendo la variable db
+from bson import json_util #transformar json porque al hacer la llamada nos da un bson mo un json. Json util nos pasa la lista a str
 from json import loads #cargar en json
 
 router = APIRouter()
 
-@router.get("/paises/{pais}")
-def nombre(pais:str):
-    q = db["paises_2"].find({"Teams": pais},{"_id":0})
+@router.get("/info/all/{pais}") #nos devuelve toda la informacion de un pais
+def allinfo(pais:str):
+    q = db["paises"].find({"Teams": pais.capitalize()},{"_id":0}) 
+    return loads(json_util.dumps(q)) #esto es porque q nos devuelve una lista y lo queremos en json
+
+@router.get("/info/name/{pais}") #nos devuelve el nombre del pais
+def name(pais:str):
+    q = db["paises"].find({"Teams": pais.capitalize()},{"_id":0, "Teams":1}) 
     return loads(json_util.dumps(q))
 
-@router.get("/paises") #en la ruta hay que escribir /paises?pais=Spain
-def nombre(pais:str):
-    q = db["paises_2"].find({"Teams": pais},{"_id":0})
+@router.get("/info/all/paises") #nos devuelve todos los nombres de los paises
+def names():
+    q = db["paises"].find({},{"_id":0, "Teams":1}) 
+    return loads(json_util.dumps(q))
+
+@router.get("/info/stage/{ronda}") #nos devuelve segun la ronda los paises eliminados
+def stage_paises(ronda:str):
+    q = db["paises"].find({"Stage": ronda.capitalize()},{"_id":0, "Teams":1}) 
     return loads(json_util.dumps(q))
 
 
-@router.get("/saludo/{name}")
-def saludo(name):
-    return {
-        "saludo": name
-    }
-@router.get("/despedida/{name}")
-def despedir(name):
-    return{
-        "despedir":name
-    }
+#@router.get("/paises") #en la ruta hay que escribir /paises?pais=Spain
+#def nombre(pais:str):
+    #q = db["paises_2"].find({"Teams": pais},{"_id":0})
+    #return loads(json_util.dumps(q))
+
