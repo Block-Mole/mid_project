3db04b0980f2840e0f117310903f440cbb5d0680
Jaime <jaimevs1993@gmail.com>
2022-11-18 11:09:33 -0600

    update

--- api/Routers/acceso.py
+++ api/Routers/acceso.py
@@ -25,10 +25,17 @@ def stage_paises(ronda:str):
     q = db["paises"].find({"Stage": ronda.capitalize()},{"_id":0, "Teams":1}) 
     return loads(json_util.dumps(q))
 
-@router.get("/info/goals_favour_per_game/{pais}") #nos todos los goles por partido de cada pais(todos)
-def goals_per_game(pais):
-    q = list(db["paises"].find({"Teams": pais.capitalize()},{"_id":0, "Goals favour per game":1}))
-    return loads(json_util.dumps(q))    
+@router.get("/info/goals_favour_per_game") #nos todos los goles por partido de cada pais(todos)
+def goals_per_game():
+    q = list(db["paises"].find({},{"_id":0, "Teams":1, "Goals favour per game":1}))
+    return loads(json_util.dumps(q))
+
+#@router.get("/equipo/goles") #sumar todos los valores dentro de un filtro
+#def todos_goles_avg():
+    #res = db["paises"].aggregate([{"$group": {"_id": "$Team", "goles_favor":{"$sum":"$Goals favour per game"}}}])
+    #return loads(json_util.dumps(res))
+
+
 
 #@router.get("/paises") #en la ruta hay que escribir /paises?pais=Spain
 #def nombre(pais:str):
